88ad0039c7e485062290939d985653e5ec93dec9
Alex Nesterenko <comfyprog@proton.me>
2023-02-03 19:54:20 +0300

    read config

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"time"
+
+	"gopkg.in/yaml.v3"
+)
+
+type PingConfig struct {
+	Size     int           `yaml:"size"`
+	Interval time.Duration `yaml:"interval"`
+}
+
+type Host struct {
+	Name       string `yaml:"name"`
+	Addr       string `yaml:"addr"`
+	PingConfig `yaml:",inline"`
+}
+
+type AppConfig struct {
+	PingConfig `yaml:",inline"`
+	Hosts      []Host `yaml:"hosts,flow"`
+}
+
+func getRawConfig(filename string) ([]byte, error) {
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return content, nil
+}
+
+func getConfig(rawConfig []byte) (AppConfig, error) {
+	var config AppConfig = AppConfig{PingConfig: PingConfig{Size: 64, Interval: time.Second}}
+	err := yaml.Unmarshal(rawConfig, &config)
+	if err != nil {
+		return config, err
+	}
+
+	for i, h := range config.Hosts {
+		if h.Size == 0 {
+			config.Hosts[i].Size = config.Size
+		}
+		if h.Interval == time.Second*0 {
+			config.Hosts[i].Interval = config.Interval
+		}
+	}
+
+	return config, nil
+
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage:\n%s CONFIG_FILE\n", os.Args[0])
+		os.Exit(1)
+	}
+	rawConfig, err := getRawConfig(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	config, err := getConfig(rawConfig)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(config)
+}
